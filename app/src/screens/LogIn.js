ec264adadf2ecb9e743b21ca30290d53ac880aff
Sergio <Sergio@iMac-de-Sergio.local>
2018-05-17 00:11:06 +0200

    navigators hechos clases y almacenado en el state los valores de los campos

--- app/src/screens/LogIn.js
+++ app/src/screens/LogIn.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import { View } from "react-native";
 import { Card, Button, FormLabel, FormInput } from "react-native-elements";
 import { onLogIn } from "../../auth";
 
-export default ({ navigation }) => (
-  <View style={{ paddingVertical: 20 }}>
-    <Card>
-      <FormLabel>Email</FormLabel>
-      <FormInput placeholder="Email address..." />
-      <FormLabel>Password</FormLabel>
-      <FormInput secureTextEntry placeholder="Password..." />
+export default class Login extends Component {
 
-      <Button
-        buttonStyle={{ marginTop: 20 }}
-        backgroundColor="#03A9F4"
-        title="LOG IN"
-        onPress={() => {
-          onLogIn().then(() => navigation.navigate("LoggedIn"));
-        }}
-      />
-    </Card>
-  </View>
-);
+  state ={
+    username: '',
+    password: '',
+  }
+
+  _onChangeText = (text, type) => this.setState({ [type]: text })
+
+  render(){
+    return(
+      <View style={{ paddingVertical: 20 }}>
+        <Card>
+          <FormLabel>User</FormLabel>
+          <FormInput 
+            placeholder="User name..." 
+            onChangeText={text => this._onChangeText(text, 'username')}
+          />
+          <FormLabel>Password</FormLabel>
+          <FormInput 
+            secureTextEntry 
+            placeholder="Password..." 
+            onChangeText={text => this._onChangeText(text, 'password')}
+            />
+
+          <Button
+            buttonStyle={{ marginTop: 20 }}
+            backgroundColor="#03A9F4"
+            title="LOG IN"
+            onPress={() => {
+              onLogIn().then(() => this.props.navigation.navigate("LoggedIn"));
+            }}
+          />
+        </Card>
+      </View>
+    )
+  }
+}
--- app/src/screens/SignUp.js
+++ app/src/screens/SignUp.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import { View } from "react-native";
 import { Card, Button, FormLabel, FormInput } from "react-native-elements";
 import { onLogIn } from "../../auth";
 
-export default ({ navigation }) => (
-  <View style={{ paddingVertical: 20 }}>
-    <Card>
-      <FormLabel>Email</FormLabel>
-      <FormInput placeholder="Email address..." />
-      <FormLabel>Password</FormLabel>
-      <FormInput secureTextEntry placeholder="Password..." />
-      <FormLabel>Confirm Password</FormLabel>
-      <FormInput secureTextEntry placeholder="Confirm Password..." />
+export default class Signup extends Component {
 
-      <Button
-        buttonStyle={{ marginTop: 20 }}
-        backgroundColor="#03A9F4"
-        title="SIGN UP"
-        onPress={() => {
-          onLogIn().then(() => navigation.navigate("LoggedIn"));
-        }}
-      />
-    </Card>
-  </View>
-);
+  state ={
+    username: '',
+    password: '',
+    email: '',
+  }
+
+  _onChangeText = (text, type) => this.setState({ [type]: text })
+
+  render (){
+    return (
+      <View style={{ paddingVertical: 20 }}>
+      <Card>
+        <FormLabel>User Name</FormLabel>
+        <FormInput 
+          placeholder="User name..." 
+          onChangeText={text => this._onChangeText(text, 'username')}
+        />
+        <FormLabel>Password</FormLabel>
+        <FormInput 
+          secureTextEntry 
+          placeholder="Password..." 
+          onChangeText={text => this._onChangeText(text, 'password')}
+        />
+        <FormLabel>Email</FormLabel>
+        <FormInput 
+          secureTextEntry 
+          placeholder="Email..." 
+          onChangeText={text => this._onChangeText(text, 'email')}
+        />
+
+        <Button
+          buttonStyle={{ marginTop: 20 }}
+          backgroundColor="#03A9F4"
+          title="SIGN UP"
+          onPress={() => {
+            onLogIn().then(() => this.props.navigation.navigate("LoggedIn"));
+          }}
+        />
+      </Card>
+    </View>
+    )
+  }
+}
