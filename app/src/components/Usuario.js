e6533ea676eee20aa712a64cc31a2853ebb8e55c
Sergio <Sergio@iMac-de-Sergio.local>
2018-06-02 13:37:58 +0200

    nuevo componente usuario

---
+++ app/src/components/Usuario.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity  } from 'react-native';
+
+class Usuario extends Component {
+  render() {
+    const {item} = this.props
+    console.log(item)
+    return (
+      <TouchableOpacity onPress={() => console.log('pulsado')}>
+        <View style={styles.container}>
+          <Image
+            style={styles.userAvatar}
+            source={require('../../assets/images/default-avatar_800x.jpg')}
+          />
+          <View style={styles.datos}>
+            <Text style={styles.textName}>{item.username}</Text>
+            <Text>@{item.alias}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  userAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey',
+    padding: 10,
+    marginBottom: 3,
+  },
+  datos: {
+    justifyContent: 'space-between'
+  },
+  textName: {
+    fontSize: 20,
+  }
+})
+
+export default Usuario
--- app/src/screens/Home.js
+++ app/src/screens/Home.js
@@ -2,18 +2,19 @@ import React, {Component} from 'react';
 import {View, FlatList} from 'react-native';
 import {graphql} from 'react-apollo';
 
-import GET_MENSAJES from '../graphql/queries/getMensajes'
-import Links from '../components/Links'
+import GET_USERS from '../graphql/queries/getUsers'
+import Usuario from '../components/Usuario'
 
 class Home extends Component {
 
-  _renderItem = ({ item }) => <Links item={item}/>
+  _renderItem = ({ item }) => <Usuario item={item}/>
 
   render(){
     const { data } = this.props
     return (
       <FlatList
-        data ={data.mensajes}
+        style={{marginTop: 30}}
+        data ={data.users}
         keyExtractor={item => item.id}
         renderItem={this._renderItem}
       />
@@ -21,4 +22,4 @@ class Home extends Component {
   }  
 }
 
-export default graphql(GET_MENSAJES)(Home)
+export default graphql(GET_USERS)(Home)
